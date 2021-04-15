312662fe1aa53a196effc2b799754b1e20460f46
Sausty45 <36990678+Sausty45@users.noreply.github.com>
2021-04-15 12:42:03 +0200

    [EXAMPLES] Updated examples

--- examples/graphics/main.cpp
+++ examples/graphics/main.cpp
@@ -3,50 +3,117 @@
 #include <memory>
 #include <SDL.h>
 #include <stdlib.h>
+#include <imgui.h>
+#include "FirstPersonCamera.h"
 
 using namespace symphony;
 
 class SymphonySandbox : public Application
 {
 public:
-	// Choose the API you want (D3D11, GL 4.5 or Vulkan)
 	SymphonySandbox()
-		: Application(RenderAPI::Vulkan, "Symphony Sandbox")
+		// Only D3D11 is available
+		: Application(RenderAPI::DirectX11, "Symphony Sandbox")
 	{
-		// Print the renderer info (DX12 not supported yet)
+		// Init the internal timer
+		InternalTimer::Init();
+
+		// Set event callback
+		m_Window->SetEventCallback(SY_BIND_EVENT_FN(SymphonySandbox::OnEvent));
+
+		// Get GPU info
 		Renderer::PrintRendererInfo();
 
-		// Clear color and load the meshes (3D models are loaded on another thread)
-		Renderer::ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		Mesh gunMesh(MeshBuilder::LoadModelDataAsync("resources/low.obj", "resources/gun_texture.png"));
-		Mesh monkeyMesh(MeshBuilder::LoadModelDataAsync("resources/suzanne.obj", "resources/marble.jpg"));
+		// Declare mesh properties and clear the renderer
+		Renderer::ClearColor(0.0156862745f, 0.54509803921f, 0.60392156862f, 1.0f);
+		Mesh stanford_dragon(MeshBuilder::LoadModelData("resources/models/dragon.obj", "resources/textures/texture.jpg"));
 
-		// Send the meshes to the renderer and prepare the renderer for drawing
-		Renderer::AddMesh(gunMesh, "Gun");
-		Renderer::AddMesh(monkeyMesh, "Suzanne");
+		MaterialUniforms ubo{};
+		ubo.Ambient = glm::vec3(0.0f, 0.0f, 0.5f);
+		ubo.Diffuse = glm::vec3(0.0f, 0.0f, 0.5f);
+		ubo.Specular = glm::vec3(0.0f, 0.0f, 0.5f);
+
+		// Add the mesh to the renderer, signal the renderer that mesh preperation is finished
+		Renderer::AddRenderObject(stanford_dragon, ubo, "Stanford Dragon");
 		Renderer::Prepare();
+
+		// Set renderer skybox and light properties
+		Renderer::SetSkybox("resources/skybox/skybox.hdr");
+		Renderer::SetLightPosition(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
 	}
 
 	void Run() override
 	{
-		// Game loop
 		while (m_Window->IsWindowOpen())
 		{
-			// Update the meshes's transform
-			glm::mat4 newMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(-25.0f, 0.5f, -70.0f)) * glm::rotate(glm::mat4(1.0f), (float)SDL_GetTicks() / 1000.0f, glm::vec3(0.0f, 1.0f, 0.0f));
-			glm::mat4 monkeyMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(1.5f, 0.3f, -5.0f)) * glm::rotate(glm::mat4(1.0f), (float)SDL_GetTicks() / 1000.0f, glm::vec3(0.0f, 1.0f, 0.0f));
-			Renderer::SetMeshTransform("Gun", newMatrix);
-			Renderer::SetMeshTransform("Suzanne", monkeyMatrix);
+			// Update the timer
+			InternalTimer::Update();
 
+			// Update camera
+			{
+				if (Input::IsKeyPressed(Key::W))
+					camera.ProcessKeyboard(FORWARD, InternalTimer::GetDeltaTime());
+				if (Input::IsKeyPressed(Key::S))
+					camera.ProcessKeyboard(BACKWARD, InternalTimer::GetDeltaTime());
+				if (Input::IsKeyPressed(Key::A))
+					camera.ProcessKeyboard(LEFT, InternalTimer::GetDeltaTime());
+				if (Input::IsKeyPressed(Key::D))
+					camera.ProcessKeyboard(RIGHT, InternalTimer::GetDeltaTime());
+			}
+
+			// Send camera information to the renderer
+			Renderer::SendCameraPosition(camera.Position);
+			Renderer::SetCamera(camera.GetViewMatrix());
+
+			// Send render object information to the renderer
+			glm::mat4 dragon_matrix = glm::translate(glm::mat4(1.0f), glm::vec3(-0.0f, -0.0f, -3.0f)) * glm::rotate(glm::mat4(1.0f), (float)SDL_GetTicks() / 1000.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+			Renderer::SetMeshTransform("Stanford Dragon", dragon_matrix);
+
+			// Present
 			m_Window->Update();
-			
-			// Draw
 			Renderer::Draw();
 		}
 	}
+
+	void OnEvent(Event& e)
+	{
+		// Dispatch different events to functions
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<MouseMovedEvent>(SY_BIND_EVENT_FN(SymphonySandbox::OnMouseMoved));
+	}
+
+	bool OnMouseMoved(MouseMovedEvent& e)
+	{
+		// Handle camera mouse movement
+
+		int xpos = e.GetX();
+		int ypos = e.GetY();
+
+		if (firstMouse)
+		{
+			lastX = xpos;
+			lastY = ypos;
+			firstMouse = false;
+		}
+
+		float xoffset = xpos - lastX;
+		float yoffset = lastY - ypos; 
+
+		lastX = xpos;
+		lastY = ypos;
+
+		camera.ProcessMouseMovement(xoffset, yoffset);
+
+		return true;
+	}
+private:
+	Camera camera = Camera(glm::vec3(0.0f, 0.0f, 3.0f));
+	float lastX = m_Window->GetWidth() / 2.0f;
+	float lastY = m_Window->GetHeight() / 2.0f;
+	bool firstMouse = true;
 };
 
-// Entry point defined in client
+// Must be defined in client
 Application* symphony::CreateApplication()
 {
 	return new SymphonySandbox();
--- examples/graphics/shaderlib/hlsl/Fragment.hlsl
+++ examples/graphics/shaderlib/hlsl/Fragment.hlsl
@@ -6,9 +6,68 @@ struct PS_INPUT
 	float4 position: SV_POSITION;
 	float3 color: COLOR;
 	float2 texcoord: TEXCOORD;
+	float3 normals: NORMALS;
+};
+
+cbuffer RendererUniforms: register(b0)
+{
+	row_major float4x4 Model;
+	row_major float4x4 View;
+	row_major float4x4 Projection;
+};
+
+cbuffer LightRendererUniforms: register(b1)
+{
+	float4 LightDirection;
+	float4 CameraPosition;
+};
+
+cbuffer MaterialRendererUniforms : register(b2)
+{
+	float3 Ambient;
+	float3 Diffuse;
+	float3 Specular;
+	float Shininess;
 };
 
 float4 PSMain(PS_INPUT input) : SV_TARGET
 {
-	return Texture.Sample(TextureSampler, input.texcoord);
+	// GAMMA CORRECTION
+
+	const float gamma = 1.0;
+	float3 hdrColor = Texture.Sample(TextureSampler, input.texcoord).rgb;
+
+	float mapX = hdrColor.x / (hdrColor.x + 1.0);
+	float mapY = hdrColor.y / (hdrColor.y + 1.0);
+	float mapZ = hdrColor.z / (hdrColor.z + 1.0);
+	float3 map1 = float3(mapX, mapY, mapZ);
+
+	map1.x = pow(abs(map1.x), 1.0 / gamma);
+	map1.y = pow(abs(map1.y), 1.0 / gamma);
+	map1.z = pow(abs(map1.z), 1.0 / gamma);
+
+	// Lighting Calculation
+	float3 camDir = normalize(input.position.xyz - CameraPosition.xyz);
+	float3 lightDir = float3(LightDirection.xyz);
+
+	// Ambient
+	float ka = 0.1;
+	float3 ambient_light = ka * Ambient;
+
+	// Diffuse
+	float kd = 0.7;
+	float amount_diffuse_light = max(0.0, dot(lightDir, input.normals));
+	float3 diffuse_light = amount_diffuse_light * Diffuse * kd;
+
+	// SPECULAR LIGHT
+	float ks = 0.5;
+	float3 reflected_light = reflect(lightDir, input.normals);
+	float amout_specular_light = pow(max(0.1, dot(reflected_light, camDir)), Shininess);
+	float3 specular_light = ks * amout_specular_light * Specular;
+
+	float3 final_light = ambient_light + diffuse_light + specular_light;
+
+	float3 final_color = final_light + map1;
+
+	return float4(final_color, 1.0);
 }
